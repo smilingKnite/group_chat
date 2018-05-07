ac85d60475a969fff4786199ff2528357bbe81c3
Tyler Mondragon <tydragon4@gmail.com>
2018-05-07 16:06:51 -0700

    made it work

--- server.js
+++ server.js
@@ -25,51 +25,72 @@ app.use(express.static(__dirname + "/stylesheet/style.css"));
 
 var users = [];
 var id = 0;
-var messages = {
-    user: null,
-    msg:[]
-}
+
 
 io.on('connection', function (socket) {
+    var messages;
+    var user;
     // emit users
     io.emit("user_data", {
-        users: users
+        users: users,
+        msgs: messages
         // emits to all new users the array of users
     });
+
     socket.on("new_user_added", function(data){
         id ++;
-        var user = {
-            user_id: id,
+        user = {
+            id: id,
             name: data.user
         };
         // creating a new user and pushing it to the users array
         users.push(user);
+        console.log(user.name + " is signing on..");
 
-        socket.on("disconnect", function(){
-            for (var i =0; i< users.length; i++) {
-                if (users[i] == user){
-                    users.splice(i, 1);
-                }
-            }
-            console.log(user);
-            io.emit("disconnect_user", {
-                user: user
-            });
-            
         io.emit("new_user", {
             user: user,
             users: users
         });
 
-        socket.emit("users_data_two", {
-            users: users,
-            user: user
-        });
-        
     });
+    socket.on("new_message", function(data){
+        messages = {
+            user: null,
+            msg: []
+        }
+        var new_mes = data.msg;
+        // console.log(data.msg);
 
+        messages.user = user;
+        messages.msg.push(new_mes);
+        io.emit("new_mes", {
+            msgs: messages
+        });
+    });
+    // socket.on("leaving_user", function(data){
+    //     var user = data.user;
+
+    //     for (i in users){
+    //         if (i == user){
+    //             // users.delete(i);
+    //             console.log("hi")
+    //         }
+    //     }
+    // });
+
+    socket.on("disconnect", function(data){
+        for (var i =0; i< users.length; i++) {
+            if (users[i] == user){
+                users.splice(i, 1);
+                console.log(user.name + " is signing off..");
+            }
+        }
+        io.emit("user_data", {
+            users: users
+        });
+        // console.log(e);
         
-        // console.log(data)/////////
+
     });
 });
 // io.on("disconnection", function(socket){
